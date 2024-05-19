765f1f668e80b95b39d3ab83ab784b0c72cb16ab
yuki-9651 <165871172+yuki-9651@users.noreply.github.com>
2024-05-19 09:05:17 +0000

    [Finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,14 @@ class ListsController < ApplicationController
   end
   
   def create
-    list = List.new(list_params)
+   @list = List.new(list_params)
+   
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :index
+    end
     
-    list.save
-    
-    redirect_to list_path(list.id)
   end
 
   def index
@@ -26,11 +29,26 @@ class ListsController < ApplicationController
   end
 
   def edit
+    
+    @list = List.find(params[:id])
+
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
   end
   
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
